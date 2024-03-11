2db7a09694a32c14ed3405bcede89570b11219fb
aman uchitkar <amanuchitkar@gmail.com>
2024-03-11 14:22:29 +0530

    60 Days of ReactJs

---
+++ textutils/module1.mjs
@@ -0,0 +1,7 @@
+const a='aman';
+const b='prem';
+const c='pol';
+const d='sharma';
+
+export default c;
+export {a,b,d}
---
+++ textutils/moudul.mjs
@@ -0,0 +1,3 @@
+import ui,{a,b,d} from "./module1.mjs"
+console.log(ui)
+console.log(a,b,d)
