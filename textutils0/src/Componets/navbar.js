3babbd41dd7dab68c75939a4da21b972318dfde8
aman uchitkar <amanuchitkar@gmail.com>
2024-03-29 01:59:05 +0530

    60 Days of ReactJs

--- textutils0/src/Componets/navbar.js
+++ textutils0/src/Componets/navbar.js
@@ -30,6 +30,7 @@ export default function Navbar(props) {
                   <Link className="nav-link active" aria-current="page" to="/">
                     Home
                   </Link>
+                  
                 </li>
                 <li className="nav-item">
                   <Link className="nav-link" to="/about">
