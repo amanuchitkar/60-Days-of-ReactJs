771a656caf12a6f1457db9d7a5c1d0a5eeaa5fdd
aman uchitkar <amanuchitkar@gmail.com>
2024-04-09 22:40:50 +0530

    60 Days of ReactJs

--- newsapp/src/components/News.js
+++ newsapp/src/components/News.js
@@ -1,20 +1,26 @@
-import React, { Component } from 'react'
-import Newsitem from './Newsitem'
+import React, { Component } from "react";
+import Newsitem from "./Newsitem";
 
 export class News extends Component {
   render() {
     return (
-      <div>
-        This is News Component
-        <Newsitem/>
-        <Newsitem/>
-        <Newsitem/>
-        <Newsitem/>
-        <Newsitem/>
-        <Newsitem/>
+      <div className="container my-3">
+        <h2>News - Top Headlines</h2>
+
+        <div className="row">
+          <div className="col-md-4">
+            <Newsitem title="cricket" description="This is desc" imgUrl="https://a4.espncdn.com/combiner/i?img=%2Fi%2Fcricket%2Fcricinfo%2F1099495_800x450.jpg" />
+          </div>
+          <div className="col-md-4">
+            <Newsitem title="cricket" description="This is desc" imgUrl="https://a4.espncdn.com/combiner/i?img=%2Fi%2Fcricket%2Fcricinfo%2F1219926_1296x729.jpg" />
+          </div>
+          <div className="col-md-4">
+            <Newsitem title="cricket" description="This is desc" imgUrl="https://a4.espncdn.com/combiner/i?img=%2Fi%2Fcricket%2Fcricinfo%2F1219926_1296x729.jpg" />
+          </div>
+        </div>
       </div>
-    )
+    );
   }
 }
 
-export default News
+export default News;
--- newsapp/src/components/Newsitem.js
+++ newsapp/src/components/Newsitem.js
@@ -1,13 +1,29 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
 export class Newsitem extends Component {
+constructor(){
+  super();
+  console.log("Hello im a constroctor")
+};
   render() {
+    let {title, description,imgUrl}=this.props;
     return (
-      <div>
-        This is a news item
+      <div className="my-3">
+        <div  className="card" style={{width: "25rem"}}>
+          <img src={imgUrl}  className="card-img-top" alt="..." />
+          <div  className="card-body">
+            <h5  className="card-title">{title}</h5>
+            <p  className="card-text">
+             {description}
+            </p>
+            <a href="/"  className="btn btn-sm btn-primary">
+              Read More
+            </a>
+          </div>
+        </div>
       </div>
-    )
+    );
   }
 }
 
-export default Newsitem
+export default Newsitem;
