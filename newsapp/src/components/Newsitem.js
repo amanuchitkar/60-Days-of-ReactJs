ded0dfec9bbc064910502010e825646c7b29f29f
aman uchitkar <amanuchitkar@gmail.com>
2024-04-13 21:12:41 +0530

    60 Days of ReactJs

--- newsapp/src/components/Newsitem.js
+++ newsapp/src/components/Newsitem.js
@@ -1,23 +1,19 @@
 import React, { Component } from "react";
 
 export class Newsitem extends Component {
-constructor(){
-  super();
-  console.log("Hello im a constroctor")
-};
 
   render() {
-    let {title, description,imgUrl}=this.props; 
+    let {title, description,imgUrl,newsUrl}=this.props; 
     return (
       <div className="my-3">
         <div  className="card" style={{width: "25rem"}}>
           <img src={imgUrl}  className="card-img-top" alt="Check your internet connection" /> 
           <div  className="card-body">
-            <h5  className="card-title">{title}</h5>
+            <h5  className="card-title">{title}...</h5>
             <p  className="card-text">
-             {description}
+             {description}...
             </p>
-            <a href="/"  className="btn btn-sm btn-primary">
+            <a href={newsUrl} target="blank" className="btn btn-sm btn-primary">
               Read More
             </a>
           </div>
