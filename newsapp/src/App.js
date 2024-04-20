1f9189837b28bf320c8550cbde415f7bc2282b16
aman uchitkar <amanuchitkar@gmail.com>
2024-04-20 14:21:59 +0530

    60 Days of ReactJs

--- newsapp/src/App.js
+++ newsapp/src/App.js
@@ -7,6 +7,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import LoadingBar from "react-top-loading-bar";
 
 export default class App extends Component {
+  
   state = {
     progress: 0,
   };
