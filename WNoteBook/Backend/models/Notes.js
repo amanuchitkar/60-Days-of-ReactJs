8a31fe95cbb65261a79038bf0df0b93c86ab40a7
aman uchitkar <amanuchitkar@gmail.com>
2024-05-10 02:19:08 +0530

    60 Days of ReactJs

--- WNoteBook/Backend/models/Notes.js
+++ WNoteBook/Backend/models/Notes.js
@@ -1,6 +1,11 @@
 const mongoose = require("mongoose");
+const { Schema } = mongoose;
 
 const NotesSchema = new Schema({
+  user:{
+    type:mongoose.Schema.Types.ObjectId,
+    ref:"user"
+  },
   titel: {
     type: String,
     require: true,
--- WNoteBook/Backend/routres/notes.js
+++ WNoteBook/Backend/routres/notes.js
@@ -1,7 +1,31 @@
 const express = require("express");
 const router = express.Router();
+const fetchuser = require("../middleware/fetchuser");
+const Notes = require("../models/Notes");
+const { body, validationResult } = require("express-validator");
 
-router.get("/", (req, res) => {
-  res.json([]);
+// Route 1: Get all the notes using: GET "/api/auth/fetchallnotes". Login required
+router.get("/fetchalluser", fetchuser, async (req, res) => {
+  const notes = await Notes.find({ user: req.user.id });
+  res.json(notes);
 });
+// Route 1: Get all the notes using: GET "/api/auth/fetchallnotes". Login required
+router.post(
+  "/addnote",
+  fetchuser,
+  [
+    body("titel", "Enter a valid titel").isLength({ min: 3 }),
+    body("description", "Description must be atleast 5 characters").isLength({
+      min: 5,
+    }),
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    res.json(notes);
+  }
+);
 module.exports = router;
